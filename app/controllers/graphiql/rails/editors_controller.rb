99bbe8aec73a819116238773987116bd67ad28a7
Brian Thomas Storti <btstorti@gmail.com>
2016-02-15 20:27:14 +0100

    Configure http method used

--- app/controllers/graphiql/rails/editors_controller.rb
+++ app/controllers/graphiql/rails/editors_controller.rb
@@ -8,6 +8,11 @@ module GraphiQL
       def graphql_endpoint_path
         params[:graphql_path] || raise(%|You must include `graphql_path: "/my/endpoint"` when mounting GraphiQL::Rails::Engine|)
       end
+
+      helper_method :graphql_endpoint_method
+      def graphql_endpoint_method
+        params[:graphql_method] || "POST"
+      end
     end
   end
 end
