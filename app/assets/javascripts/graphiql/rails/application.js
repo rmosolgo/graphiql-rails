d2439f00800620b94d476168834bffcce72190e3
Jared Armstrong <jared.armstrong@gmail.com>
2016-11-13 21:57:09 +0000

    update graphql to 0.8.0

--- app/assets/javascripts/graphiql/rails/application.js
+++ app/assets/javascripts/graphiql/rails/application.js
@@ -1,4 +1,4 @@
-//= require ./react-15.3.1
-//= require ./react-dom-15.3.1
+//= require ./react-15.3.2
+//= require ./react-dom-15.3.2
 //= require ./fetch-0.10.1
-//= require ./graphiql-0.7.4
+//= require ./graphiql-0.8.0
