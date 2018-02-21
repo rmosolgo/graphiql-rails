3eb2491475823d332c314611e73288200998c566
Grzegorz Jesionowski <grzegorz.jesionowski@elpassion.com>
2018-02-21 15:05:59 +0100

    Update graphiql js libs in order to serve new features

--- app/assets/javascripts/graphiql/rails/application.js
+++ app/assets/javascripts/graphiql/rails/application.js
@@ -1,4 +1,4 @@
-//= require ./react-16.0.0
-//= require ./react-dom-16.0.0
+//= require ./react-16.2.0
+//= require ./react-dom-16.2.0
 //= require ./fetch-0.10.1
-//= require ./graphiql-0.11.10
+//= require ./graphiql-0.11.11
