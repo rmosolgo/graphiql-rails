f1fbc48877514ea670f997090011c76f8df6644f
gregology <greg@gho.st>
2018-07-16 15:13:47 -0400

    adding title and logo options

--- test/controllers/editors_controller_test.rb
+++ test/controllers/editors_controller_test.rb
@@ -10,6 +10,8 @@ module GraphiQL
       teardown do
         GraphiQL::Rails.config.query_params = false
         GraphiQL::Rails.config.initial_query = nil
+        GraphiQL::Rails.config.title = nil
+        GraphiQL::Rails.config.logo = nil
         GraphiQL::Rails.config.headers = {}
       end
 
@@ -33,6 +35,26 @@ module GraphiQL
         GraphiQL::Rails.config.initial_query = nil
         get :show, graphql_params
         refute_includes(@response.body, '"{ customQuery(id: \"123\") }"')
+      end
+
+      test "it uses title config" do
+        GraphiQL::Rails.config.title = 'Custom Title'
+        get :show, graphql_params
+        assert_includes(@response.body, '<title>Custom Title</title>')
+
+        GraphiQL::Rails.config.title = nil
+        get :show, graphql_params
+        assert_includes(@response.body, '<title>GraphiQL</title>')
+      end
+
+      test "it uses logo config" do
+        GraphiQL::Rails.config.logo = 'Custom Logo'
+        get :show, graphql_params
+        assert_includes(@response.body, 'React.createElement(GraphiQL.Logo, {}, "Custom Logo")')
+
+        GraphiQL::Rails.config.logo = nil
+        get :show, graphql_params
+        refute_includes(@response.body, 'React.createElement(GraphiQL.Logo, {}, "Custom Logo")')
       end
 
       test "it uses query_params config" do
