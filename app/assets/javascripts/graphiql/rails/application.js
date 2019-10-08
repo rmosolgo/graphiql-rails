427f358c0fa15135b086521326aa9b9c90172890
Ramin B <ramin@wework.com>
2019-10-08 12:20:16 -0400

    update graphiql dependencies

--- app/assets/javascripts/graphiql/rails/application.js
+++ app/assets/javascripts/graphiql/rails/application.js
@@ -1,5 +1,5 @@
-//= require ./react-16.7.0
-//= require ./react-dom-16.7.0
+//= require ./react-16.10.2
+//= require ./react-dom-16.10.2
 //= require ./fetch-0.10.1
-//= require ./graphiql-0.12.0
+//= require ./graphiql-0.15.1
 //= require ./graphiql_show
