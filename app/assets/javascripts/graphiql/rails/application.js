59698a4a7035849359b9fec691745070407fa511
Tejas Dinkar <tejas@gja.in>
2021-08-17 12:26:58 +0530

    run `rake update_graphiql`

--- app/assets/javascripts/graphiql/rails/application.js
+++ app/assets/javascripts/graphiql/rails/application.js
@@ -1,5 +1,5 @@
-//= require ./react-16.7.0
-//= require ./react-dom-16.7.0
+//= require ./react-16.14.0
+//= require ./react-dom-16.14.0
 //= require ./fetch-0.10.1
-//= require ./graphiql-0.12.0
+//= require ./graphiql-1.4.2
 //= require ./graphiql_show
