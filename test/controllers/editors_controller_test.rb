681b8af1991be1cbe1e2905bb158e4163f65e190
Robert Mosolgo <rmosolgo@github.com>
2021-08-17 09:27:01 -0400

    update tests, add ci

--- test/controllers/editors_controller_test.rb
+++ test/controllers/editors_controller_test.rb
@@ -20,7 +20,7 @@ module GraphiQL
       end
 
       test 'renders GraphiQL' do
-        get :show, graphql_params
+        get :show, **graphql_params
         assert_response(:success)
         assert_includes(@response.body, 'my/endpoint', 'it uses the provided path')
         assert_match(/application-\w+\.js/, @response.body, 'it includes assets')
@@ -28,46 +28,46 @@ module GraphiQL
 
       test 'it uses initial_query config' do
         GraphiQL::Rails.config.initial_query = '{ customQuery(id: "123") }'
-        get :show, graphql_params
+        get :show, **graphql_params
         assert_includes(@response.body, '"{ customQuery(id: &quot;123&quot;) }"')
 
         GraphiQL::Rails.config.initial_query = nil
-        get :show, graphql_params
+        get :show, **graphql_params
         refute_includes(@response.body, '"{ customQuery(id: &quot;123&quot;) }"')
       end
 
       test 'it uses title config' do
         GraphiQL::Rails.config.title = 'Custom Title'
-        get :show, graphql_params
+        get :show, **graphql_params
         assert_includes(@response.body, '<title>Custom Title</title>')
 
         GraphiQL::Rails.config.title = nil
-        get :show, graphql_params
+        get :show, **graphql_params
         assert_includes(@response.body, '<title>GraphiQL</title>')
       end
 
       test 'it uses logo config' do
         GraphiQL::Rails.config.logo = 'Custom Logo'
-        get :show, graphql_params
+        get :show, **graphql_params
         assert_includes(@response.body, %(data-logo="Custom Logo"))
 
         GraphiQL::Rails.config.logo = nil
-        get :show, graphql_params
+        get :show, **graphql_params
         refute_includes(@response.body, %(data-logo="Custom Logo"))
       end
 
       test 'it uses query_params config' do
-        get :show, graphql_params
+        get :show, **graphql_params
         assert_includes(@response.body, %(data-query-params="false"))
 
         GraphiQL::Rails.config.query_params = true
-        get :show, graphql_params
+        get :show, **graphql_params
         assert_includes(@response.body, %(data-query-params="true"))
       end
 
       test 'it renders headers' do
         GraphiQL::Rails.config.headers['Nonsense-Header'] = ->(_view_ctx) { 'Value' }
-        get :show, graphql_params
+        get :show, **graphql_params
         assert_includes(@response.body, %(&quot;Nonsense-Header&quot;:&quot;Value&quot;))
         assert_includes(@response.body, %(&quot;X-CSRF-Token&quot;:&quot;))
       end
