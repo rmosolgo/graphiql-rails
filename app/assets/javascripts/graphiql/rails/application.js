4ef5a58cf6a35f1078fc3deba36eefd8fee51148
Tieg Zaharia <tieg@tidelift.com>
2023-03-03 10:01:27 -0700

    Update graphiql assets from 1.4.2 to 2.4.0

--- app/assets/javascripts/graphiql/rails/application.js
+++ app/assets/javascripts/graphiql/rails/application.js
@@ -1,5 +1,5 @@
-//= require ./react-16.14.0
-//= require ./react-dom-16.14.0
+//= require ./react-17.0.2
+//= require ./react-dom-17.0.2
 //= require ./fetch-0.10.1
-//= require ./graphiql-1.4.2
+//= require ./graphiql-2.4.0
 //= require ./graphiql_show
