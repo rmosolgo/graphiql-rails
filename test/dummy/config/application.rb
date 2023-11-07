96e65de14cf2205a0fa9a96e3b8a7d90c1e38318
scott-knight <dc.sknight@gmail.com>
2023-11-06 18:34:36 -0600

    fix the gem to use assets without sprokets

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
